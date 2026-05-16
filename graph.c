6efc2897d35b87fa495050f242d91652de477cfc
Your Name <you@example.com>
2026-05-16 21:31:31 +0000

    new try

--- graph.c
+++ graph.c
@@ -35,7 +35,7 @@ Graph* createGraph() {
 
 void addNode(Graph* g, const char* label) {
     if (!g || !label) return;
-    if (map_search(g->adjacencyMap, (void *) label) != NULL) return NULL;
+    if (map_search(g->adjacencyMap, (void *) label) != NULL) return;
     char* copiaLabel = strdup(label);
     List* lista = list_create();
     map_insert(g->adjacencyMap, copiaLabel, lista);
